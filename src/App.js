03300a6d09257a0637577bb155a28942aa951804
Monika Sharma <monika2004sharma@gmail.com>
2021-06-12 04:30:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import AppNavbar from './components/navbar';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    images : []
+  }
+
+  componentDidMount(){
+    axios
+      .get('http://localhost:4000/api/files')
+      .then(res => {
+          console.log(res)
+          this.setState({
+            images : res.data.images
+          })
+      })
+  }
+
+  onDelete = id => {
+      axios
+        .delete(`http://localhost:4000/api/file/`+id)
+        .then(res => {
+          if(res.data.success){
+            window.location.reload()
+          }
+        })
+  }
+
+  render() { 
+
+    return ( 
+      <div>
+          <AppNavbar/>
+          <br/><br/>
+
+          <div className="container row">
+
+          {
+            this.state.images.length === 0 ? (
+              <h4>You have not uploaded any images</h4>
+            ) : (
+              this.state.images.map(img => {
+              let address = 'http://localhost:4000/api/'+img.address;
+            
+              return(
+                <div key={img._id}>
+                  <div className="col s6">
+                    <div className="card">
+                      <div className="card-image">
+                        <img src={address}/>
+                        <a className="btn-floating halfway-fab waves-effect waves-light red">
+                          
+                        <button onClick={() => this.onDelete(img._id)} style={{
+                            backgroundColor : "transparent",
+                            border : 0
+                          }}>
+                          <i className="material-icons">
+                            delete
+                          </i>
+                        </button>
+                        </a>
+                      </div>
+                      <div className="card-content">
+                        <p>Image Id : {img._id}</p>
+                      </div>
+                    </div>
+                  </div>
+                  </div>
+              )
+
+            }
+            )
+          )
+        }
+
+        </div>
+      </div>
+     );
+  }
 }
-
+ 
 export default App;
